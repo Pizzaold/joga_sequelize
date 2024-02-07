e3e8a6aadc9e5358df27e819057a5ff7ebde0366
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-02-07 11:21:39 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://foo:bar@192.168.27.199:3306/joga_sequliize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Kurat Miku:', err);
+    });
+
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the application.' });
 });
