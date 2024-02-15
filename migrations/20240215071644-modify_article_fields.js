c845c299f0ef04077dd5c7c046309349b001d0ea
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-02-15 09:19:59 +0200

    Created article table and added table data

--- migrations/20240215071644-modify_article_fields.js
+++ migrations/20240215071644-modify_article_fields.js
@@ -1,42 +1,42 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up (queryInterface, Sequelize) {
     return Promise.all([
     queryInterface.changeColumn('Articles', 'id', {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
     }),
     queryInterface.changeColumn('Articles', 'name', {
-      type: Sequelize.STRING,
-      allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false
     }),
     queryInterface.changeColumn('Articles', 'slug', {
-      type: Sequelize.STRING,
-      allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
     }),
     queryInterface.changeColumn('Articles', 'image', {
-      type: Sequelize.STRING,
-      allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false
     }),
     queryInterface.changeColumn('Articles', 'body', {
-      type: Sequelize.TEXT,
-      allowNull: false
+    type: Sequelize.TEXT,
+    allowNull: false
     }),
     queryInterface.changeColumn('Articles', 'published', {
-      type: Sequelize.DATE,
-      allowNull: false
+    type: Sequelize.DATE,
+    allowNull: false
     }),
     queryInterface.changeColumn('Articles', 'author_id', {
-      type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER
     })
     ])
   },
 
-  down (queryInterface) {
+  down (queryInterface, Sequelize) {
     return Promise.all([
     queryInterface.dropTable('Articles')
     ])
