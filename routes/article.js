39df142f9f16952e36d9442a48286d7dfbaabcd7
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-03-05 09:09:22 +0200

    Add route for updating articles in admin panel

--- routes/article.js
+++ routes/article.js
@@ -7,6 +7,7 @@ router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
 
 router.post('/admin/article/create', articleAdminController.createArticle);
+router.get('/admin/article/edit/:id', articleAdminController.updateArticle);
 router.post('/admin/article/edit/:id', articleAdminController.updateArticle);
 
 module.exports = router;
